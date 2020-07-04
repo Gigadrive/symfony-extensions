950cc2383ebd2e9b5a5ad31aa3a18fda13a25fe8
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2020-07-04 09:51:53 +0200

    Fixed wrong namespace for CacheConstants

--- src/Constants/CacheConstants.php
+++ src/Constants/CacheConstants.php
@@ -17,7 +17,7 @@
  * along with this program. If not, see <https://gnu.org/licenses/>
  */
 
-namespace Gigadrive\Bundle\SymfonyExtensionsBundle\Cache;
+namespace Gigadrive\Bundle\SymfonyExtensionsBundle\Constants;
 
 class CacheConstants {
 	/**
